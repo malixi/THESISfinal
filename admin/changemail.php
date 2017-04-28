a017b08f0d25aadeafd27601a81e40b467d07573
khellytaguinod <khellytaguinod@gmail.com>
2017-04-28 21:42:14 +0800

    Fix index linking
    - replaced all the index.html to index.php

--- admin/changemail.php
+++ admin/changemail.php
@@ -70,7 +70,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         <!-- Top Navigation: Left Menu -->
         <ul class="nav navbar-nav navbar-left navbar-top-links">
-            <li><a href="../index.html"><i class="fa fa-home fa-fw"></i> Website</a></li>
+            <li><a href="../index.php"><i class="fa fa-home fa-fw"></i> Website</a></li>
         </ul>
 
         <!-- Top Navigation: Right Menu -->

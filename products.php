6c1385367926076e8d1494114494261be13c0380
ZGozon <201501221@iacademy.edu.ph>
2017-04-30 16:48:51 +0800

    converted to php

---
+++ products.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mattress Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--fonts-->
+<link href='//fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'><!--//fonts-->
+<!-- start menu -->
+<link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/memenu.js"></script>
+<script>$(document).ready(function(){$(".memenu").memenu();});</script>
+<script src="js/simpleCart.min.js"> </script>
+</head>
+<body>
+<!--header-->
+<?php include 'header.php';    ?>
+
+<!-- search-scripts -->
+					<script src="js/classie.js"></script>
+					<script src="js/uisearch.js"></script>
+						<script>
+							new UISearch( document.getElementById( 'sb-search' ) );
+						</script>
+					<!-- //search-scripts -->
+
+				<div class="ca-r">
+					<div class="cart box_1">
+						<a href="checkout.html">
+						<h3> <div class="total">
+							<span class="simpleCart_total"></span> </div>
+							<img src="images/cart.png" alt=""/></h3>
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+
+					</div>
+				</div>
+					<div class="clearfix"> </div>
+			</div>
+
+		</div>
+		</div>
+		<?php include 'navbar.php';  ?>
+<!-- products -->
+	<!-- grow -->
+	<div class="grow">
+		<div class="container">
+			<h2>Products</h2>
+		</div>
+	</div>
+	<!-- grow -->
+	<div class="pro-du">
+		<div class="container">
+			<div class="col-sm-4">
+				<div class="thumbnail">
+				<div class="grid-top  simpleCart_shelfItem">
+					<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/samplebanana.png" width="50%" alt="">
+						<div class="b-wrapper">
+							<h3 class="b-animate b-from-left    b-delay03 ">
+								<span>BANANA</span>
+
+							</h3>
+						</div>
+					</a>
+
+
+				<p><a href="single.html"><center>BANANA</center></a></p>
+					<center><a href="#" class="add-cart item_add">SEE MORE</a></center>
+
+
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="thumbnail">
+			<div class="grid-top  simpleCart_shelfItem">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/samplecocoa.gif" width="50%" alt="">
+					<div class="b-wrapper">
+						<h3 class="b-animate b-from-left    b-delay03 ">
+							<span>COCOA</span>
+
+						</h3>
+					</div>
+				</a>
+
+
+			<p><a href="single.html"><center>COCOA</center></a></p>
+			&nbsp;&nbsp;
+				<center><a href="#" class="add-cart item_add">SEE MORE</a></center>
+
+
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="thumbnail">
+		<div class="grid-top  simpleCart_shelfItem">
+			<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/samplecocnut.png" width="50%" alt="">
+				<div class="b-wrapper">
+					<h3 class="b-animate b-from-left    b-delay03 ">
+						<span>COCONUT</span>
+
+					</h3>
+				</div>
+			</a>
+
+
+		<p><a href="single.html"><center>COCONUT</center></a></p>
+
+			<center><a href="#" class="add-cart item_add">SEE MORE</a></center>
+
+
+		</div>
+	</div>
+</div>
+<div class="col-sm-4">
+	<div class="thumbnail">
+	<div class="grid-top  simpleCart_shelfItem">
+		<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/sampleherbal.png" width="50%" alt="">
+			<div class="b-wrapper">
+				<h3 class="b-animate b-from-left    b-delay03 ">
+					<span>HERBAL</span>
+
+				</h3>
+			</div>
+		</a>
+
+
+	<p><a href="single.html"><center>HERBAL</center></a></p>
+
+		<center><a href="#" class="add-cart item_add">SEE MORE</a></center>
+
+
+	</div>
+</div>
+</div>
+<div class="col-sm-4">
+	<div class="thumbnail">
+	<div class="grid-top  simpleCart_shelfItem">
+		<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/samplemango.png" width="50%" alt="">
+			<div class="b-wrapper">
+				<h3 class="b-animate b-from-left    b-delay03 ">
+					<span>MANGO</span>
+
+				</h3>
+			</div>
+		</a>
+
+&nbsp;
+	<p><a href="single.html"><center>MANGO</center></a></p>
+	&nbsp;
+		<center><a href="#" class="add-cart item_add">SEE MORE</a></center>
+
+
+	</div>
+</div>
+</div>
+<div class="col-sm-4">
+	<div class="thumbnail">
+	<div class="grid-top  simpleCart_shelfItem">
+		<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/samplepineapple.png" width="50%" alt="">
+			<div class="b-wrapper">
+				<h3 class="b-animate b-from-left    b-delay03 ">
+					<span>PINEAPPLE</span>
+
+				</h3>
+			</div>
+		</a>
+
+
+	<p><a href="single.html"><center>PINEAPPLE</center></a></p>
+	&nbsp;&nbsp;
+		<center><a href="#" class="add-cart item_add">SEE MORE</a></center>
+
+
+	</div>
+</div>
+</div>
+
+
+
+						<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!----->
+</div>
+<!---->
+</div>
+<!-- products -->
+<?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.min.js"></script>
+	<!-- Custom Theme files -->
+	<!--theme-style-->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!--//theme-style-->
+
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!--fonts-->
+	<link href='//fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'><!--//fonts-->
+	<!-- start menu -->
+	<link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+	<script type="text/javascript" src="js/memenu.js"></script>
+	<script>$(document).ready(function(){$(".memenu").memenu();});</script>
+	<script src="js/simpleCart.min.js"> </script>
+	</head>
+
+
+
+<body>
+	<!--header-->
+	<?php include 'header.php';?>
+	<!-- search-scripts -->
+						<script src="js/classie.js"></script>
+						<script src="js/uisearch.js"></script>
+							<script>
+								new UISearch( document.getElementById( 'sb-search' ) );
+							</script>
+						<!-- //search-scripts -->
+
+					<div class="ca-r">
+						<div class="cart box_1">
+							<a href="checkout.html">
+							<h3> <div class="total">
+								<span class="simpleCart_total"></span> </div>
+								<img src="images/cart.png" alt=""/></h3>
+							</a>
+							<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+
+						</div>
+					</div>
+						<div class="clearfix"> </div>
+				</div>
+
+			</div>
+			</div>
+			<div class="container">
+	<?php include 'navbar.php'; ?>
+
+
+<br><br><br>
+<div class="container-fluid">
+<div class="row">
+  <div class="col-xs-6">
+
+    <ul class="demo-3">
+    <li>
+        <figure>
+            <img src="images/service2.png" alt=""/>
+            <figcaption>
+                <br><br><br><br><br>
+                <h1>Acts as Buying Office</h1>
+                <br>
+                <h5>Our office can act as your buying office, as your third party counterpart to ensure that you are buying the exact requirement from your. Philippine supplier. We shall check the volume, quality, time of delivery and capabilities of your prospective supplier or existing supplier.</h5>
+            </figcaption>
+        </figure>
+    </li>
+</ul>
+
+  </div>
+  <div class="col-xs-6">
+
+    <ul class="demo-3">
+    <li>
+        <figure>
+            <img src="images/service2.png" alt=""/>
+            <figcaption>
+                <br><br><br><br>
+                <h1>Consolidator and booking coordinator</h1>
+                <br>
+                <h5>Our office can act as your consolidator and manage different products from different suppliers to pull cargo into one container before dispatching to the pier. We can also stand as your broker for all the documentations and as your booking coordinator to your preferred shipping lines or air lines.</h5>
+            </figcaption>
+        </figure>
+    </li>
+</ul>
+
+  </div>
+</div>
+</div>
+
+
+<br><br><br>
+
+<?php include 'footer.php'; ?>
+</body>
+</html>

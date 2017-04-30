311b6fe555c003c96767510a8bc734fc19fa8ce6
ZGozon <201501221@iacademy.edu.ph>
2017-04-30 22:53:58 +0800

    fixed links

--- about.php
+++ about.php
@@ -51,46 +51,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 		</div>
 		<div class="container">
-			<div class="head-top">
-				<div class="logo">
-					<a href="index.php">
-<img src="images/logo.png" class="navbar-brand">
-</a>
-				</div>
-
-		  <div class=" h_menu4">
-&nbsp;&nbsp;&nbsp;
-				<ul class="memenu skyblue">
-					<li><a class="color8" href="about.html"><strong>ABOUT US</strong></a></li>
-						<li><a class="color1" href="products.html">PRODUCTS</a>
-							<div class="mepanel">
-					<div class="row">
-						<div class="col1">
-							<div class="h_nav">
-								<ul>
-									<li><a href="bananaproducts.html">Banana</a></li>
-									<li><a href="cocoaproducts.html">Cocoa</a></li>
-									<li><a href="coconutproducts.html">Coconut</a></li>
-									<li><a href="herbalproducts.html">Herbal</a></li>
-									<li><a href="mangoproducts.html">Mango</a></li>
-									<li><a href="pineappleproducts.html">Pineapple</a></li>
-
-								</ul>
-							</div>
-						</div>
-
-				</li>
-
-			<li><a class="color4" href="services.html">SERVICES</a></li>
-			<li><a class="color6" href="contact.html">CONTACT</a></li>
-			</ul>
-		</div>
-
-				<div class="clearfix"> </div>
-		</div>
-		</div>
-	</div>
-</nav>
+			<?php include 'navbar.php'; ?>
 
 
 

f5e38573958a312e89e308c04faee1178d5c8c9f
James William M. Tarrobal <james.tarrobal.21@gmail.com>
2017-05-02 17:33:51 +0800

    Updating Search

--- admin/searchproductpage.php
+++ admin/searchproductpage.php
@@ -202,6 +202,7 @@ $search = $_GET['search'];
                 echo "<table class='table table.bordered'>
                     <thead>
                         <tr>
+                        	<th>Product Image</th>
                             <th>Product ID</th>
                             <th>Product Name</th>
                             <th>Product Date Created</th>
@@ -215,20 +216,31 @@ $search = $_GET['search'];
                 while($row = mysqli_fetch_array($results)){
                     echo
                         "<tr>
-                                <td>" .$row['productID']. "</td>
-                                <td>" .$row['name']. "</td>
-                                <td>" .$row['date_created']. "</td>
-                                <td>
+                            <td><img id='prodImg' src='productimage/".$row['image']."' width='100px' heigh=''/></td>
+                            <td>" .$row['productID']. "</td>
+                            <td>" .$row['name']. "</td>
+                            <td>" .$row['date_created']. "</td>
+                            <td>
+                                <div>
                                     <form method='POST' action='editproductpage.php'>
                                         <input type='hidden' name='PNAME' value='".$row['productID']."' />
-                                        <input type='submit' class='btn btn-primary col-lg-6' value='Edit' name='submit'>
+                                        <input type='submit' class='btn btn-warning col-lg-4' value='Edit' name='submit'>
                                     </form>
+                                </div>
+                                <div>
                                     <form method='POST' action='delproductprocess.php'>
                                         <input type='hidden' name='PNAME' value='".$row['productID']."' />
-                                        <input type='submit' class='btn btn-primary col-lg-6' value='Delete' name='submit'>
+                                        <input type='submit' class='btn btn-danger col-lg-4' value='Delete' name='submit' Onclick=\"return ConfirmDelete()\">
                                     </form>
-                                </td>
-                            </tr>";
+                                </div>
+								<div>
+										<form method='POST' action='product.php'>
+												<input type='hidden' name='PNAME' value='".$row['productID']."' />
+												<input type='submit' class='btn btn-primary col-lg-4' value='View' name='submit'>
+										</form>
+								</div>
+                            </td>
+                        </tr>";
                     }
                 }
 

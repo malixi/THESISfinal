8fb2fa6b0c26956ea405ae1c879c1350567fe071
malixi <jcmalixi@outlook.com>
2017-05-08 09:25:15 +0800

    Updated Password thingy
    Added old password validation

--- admin/changepassprocess.php
+++ admin/changepassprocess.php
@@ -5,36 +5,33 @@
 
 
 	$passid=$_SESSION['userSession'];
+	$oldpassword=md5($_POST['oldPassword']);
 	$newpassword=$_POST['newpassword'];
 	$newpassword2=$_POST['newpassword2'];
 	$encryptednewpassword = md5($_POST['newpassword']);
 
 
-	$stmt = $dbconn->prepare('SELECT * FROM admin WHERE userID = ?');
-	$stmt->bind_param('s', $passid);
+	$stmt = $dbconn->prepare('SELECT * FROM admin WHERE userID = ? AND userPass = ?');
+	$stmt->bind_param('is', $passid, $oldpassword);
 	$stmt->execute();
 	$result = $stmt->get_result();
 
 
-	if($newpassword !=$newpassword2)
-	{
+	if($rows = $result->fetch_assoc()){
+		if($newpassword == $newpassword2){
+			$stmt2 = $dbconn->prepare('UPDATE admin SET userPass =? WHERE userID = ?');
+			$stmt2->bind_param('si', $encryptednewpassword, $passid);
+			$stmt2->execute();
 
-		echo"<script>window.alert('Password not match');</script>";
-		echo"<script>location.href='changepass.php';</script>";
+			echo "<script>window.alert('Password updated.');</script>";
+			echo"<script>location.href='changepass.php';</script>";
+		}else{
+			echo "<script>alert('Password Dont Match.');history.back();</script>";
+		}
+	}else{
+		header("Content-Type: text/html; charset=UTF-8");
+		echo "<script>alert('Old password incorrect.');history.back();</script>";
+		$stmt->close();
+		exit;
 	}
-	else if
-	 ($rows = $result->fetch_assoc()) {
-		$stmt2 = $dbconn->prepare('UPDATE admin SET userPass = ? WHERE userID = ?');
-		$stmt2->bind_param('si', $encryptednewpassword, $passid);
-		$stmt2->execute();
-
-		echo"<script>window.alert('Password updated.');</script>";
-		echo"<script>location.href='home.php';</script>";
-}
- else {
-		echo"<script>window.alert('Product Code incorrect');</script>";
-		echo"<script>location.href='home.php';</script>";
-	}
-
-
 ?>

ee49c884932de8894e6d6a0d34c3df6c3b0f2527
James William M. Tarrobal <james.tarrobal.21@gmail.com>
2017-05-13 01:57:38 +0800

    Paypal Almost

--- view_cart.php
+++ view_cart.php
@@ -61,6 +61,28 @@ include_once("configuration.php");
 $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 echo $current_url; ?>" />
 </form>
+<?php 
+	$product_name = $cart_itm["product_name"];
+	$product_qty = $cart_itm["product_qty"];
+	$product_price = $cart_itm["product_price"];
+	$product_code = $cart_itm["product_code"];
+	$subtotal = ($product_price * $product_qty);
+echo '<form method="POST" action="https://www.sandbox.paypal.com/cgi-bin/webscr">
+	<input type="hidden" name="business" value="carlolo@gmail.com">
+	<input type="hidden" name="cmd" value="_xclick">
+	<input type="hidden" name="item_name" value="'.$product_name.'">
+	<input type="hidden" name="item_number" value="'.$product_code.'">
+	<input type="hidden" name="amount" value="'.$product_price.'">
+	<input type="hidden" name="currency_code" value="USD">
+
+	<input type="hidden" name="cancel_return" value="http://localhost/paypal_integration_php/cancel.php">
+    <input type="hidden" name="return" value="http://localhost/paypal_integration_php/success.php">
+
+    <input type="image" name="submit" border="0" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" alt="PayPal - The safer, easier way to pay online">
+    <img alt="" border="0" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif">'
+?>
+
+</form>
 </div>
 
 </body>

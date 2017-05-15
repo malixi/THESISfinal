d5c67be4f970a8888ea32e347623385d8612d8a2
James William M. Tarrobal <james.tarrobal.21@gmail.com>
2017-05-15 15:51:38 +0800

    Testing

--- configuration.php
+++ configuration.php
@@ -8,8 +8,8 @@ $db_host = 'localhost';
 
 $shipping_cost      = 1.50; //shipping cost
 $taxes              = array( //List your Taxes percent here.
-                            'VAT' => 12,
-                            'Service Tax' => 5
+                            'VAT' => 0,
+                            'Service Tax' => 0
                             );
 //connect to MySql
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);

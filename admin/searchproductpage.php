b97a4fa984ec62aed19e67d7584b38a7e2dff67d
malixi <jcmalixi@outlook.com>
2017-05-15 17:12:54 +0800

    Updated search productpage

--- admin/searchproductpage.php
+++ admin/searchproductpage.php
@@ -186,22 +186,25 @@ $search = $_GET['search'];
                     </thead>
                     <tbody>";
 
-                if($results->num_rows > 0) {
-                while($row = mysqli_fetch_array($results)){
-                    echo
-                            "<tr>
-                                <td><img id='prodImg' src='productimage/".$row['image']."' width='100px' heigh=''/></td>
-                                <td>" .$row['product_code']. "</td>
-                                <td>" .$row['name']. "</td>
-                                <td>" .$row['date_created']. "</td>
-                                <td>
-                                    <div>
-                                        <form method='POST' action='editproductpage.php'>
-                                            <input type='hidden' name='PNAME' value='".$row['productID']."' />
-                                            <input type='submit' class='btn btn-warning col-lg-4' value='Edit' name='submit'>
-                                        </form>
-                                    </div>
-                                    <div>
+                    if($results->num_rows > 0) {
+                        while($row = mysqli_fetch_array($results)){
+                            echo
+                                "<tr>
+                                    <td>";
+
+    																if($row['image'] == NULL){
+    		                                echo "
+    		                                <img id='prodImg' src='productimage/default.png' width='100px' heigh=''/></td>";
+    		                            } else{
+    																	echo "
+    																<img id='prodImg' src='productimage/".$row['image']."' width='100px' heigh=''/></td>";
+    															}
+    															echo "
+                                    <td>" .$row['product_code']. "</td>
+                                    <td>" .$row['name']. "</td>
+                                    <td>" .$row['date_created']. "</td>
+                                    <td>
+                                        <div>
                                         <form method='POST' action='delproductprocess.php'>
                                             <input type='hidden' name='PNAME' value='".$row['productID']."' />
                                             <input type='submit' class='btn btn-danger col-lg-4' value='Delete' name='submit' Onclick=\"return ConfirmDelete()\">
